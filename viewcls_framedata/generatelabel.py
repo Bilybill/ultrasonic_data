dde84c9f92d2c0bfab84cdbdef832b2697115572
Bilybill <yonghao-16@mails.tsinghua.edu.cn>
2021-11-20 21:41:04 +0800

    add another type of data

--- viewcls_framedata/generatelabel.py
+++ viewcls_framedata/generatelabel.py
@@ -3,9 +3,58 @@ import enum
 import json
 from json import encoder
 
-from ultrasonic.ultrasonic_diagnosis.dataset import label
+# from ultrasonic.ultrasonic_diagnosis.dataset import label
 with open("annotation.json",'r') as f:
     info_dic = json.load(f)
+
+#%%
+'''
+generate classification data only for echocardiogram
+'''
+echo_info_list = [item for item in info_dic if item['ustype'] == 'Echocardiogram']
+echo_view_list = [item['view'] for item in echo_info_list]
+echo_view  = set(echo_view_list)
+echo_view_2_label = {views:label_index for label_index,views in enumerate(echo_view)}
+echo_label_2_view = {label_index:views for views,label_index in echo_view_2_label.items()}
+label_list = [echo_view_2_label[item['view']] for item in echo_info_list]
+import collections
+label_count = collections.Counter(label_list)
+numdic = {echo_label_2_view[number]:value for number,value in label_count.items()}
+print(numdic)
+#%%
+
+import numpy as np
+X_train = []
+X_test = []
+y_train = []
+y_test = []
+
+for view in echo_view:
+    temp_view_list = [item for item in echo_info_list if item['view'] == view]
+    print(f'{view}:{len(temp_view_list)}')
+    view_num = len(temp_view_list)
+    random_series = np.random.permutation(view_num)
+    split = int(view_num * 1 / 10 + 0.5)
+    if view_num > 5:
+        split += 1
+    for index in random_series[:split]:
+        X_test.append(temp_view_list[index])
+        y_test.append(echo_view_2_label[temp_view_list[index]['view']])
+    # have_test_data.append(species) if split > 0 else None
+    for index in random_series[split:]:
+        X_train.append(temp_view_list[index])
+        y_train.append(echo_view_2_label[temp_view_list[index]['view']])
+
+train_count = collections.Counter(y_train)
+traindic = {echo_label_2_view[number]:value for number,value in train_count.items()}
+print(traindic)
+test_count = collections.Counter(y_test)
+testdic = {echo_label_2_view[number]:value for number,value in test_count.items()}
+print(testdic)
+#%%
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(echo_info_list,label_list,test_size=0.1,stratify=label_list)
+
 # %%
 info_dic.__len__()
 # %%
@@ -49,27 +98,27 @@ labeldic.keys()-testdic.keys()
 # %%
 import json
 import collections
-with open("train_annotation_v2.json",'w',encoding='utf-8') as f:
+with open("echotrain_annotation.json",'w',encoding='utf-8') as f:
     f.write(json.dumps(X_train,sort_keys=False,indent=4,separators=(",",": "),ensure_ascii=False)+"\n")
-with open("test_annotation_v2.json",'w',encoding='utf-8') as f:
+with open("echotest_annotation.json",'w',encoding='utf-8') as f:
     f.write(json.dumps(X_test,sort_keys=False,indent=4,separators=(",",": "),ensure_ascii=False)+"\n")
 # %%
 # dic_label = {'胸骨旁二尖瓣短轴': 0,'胸骨旁短轴心尖': 0,'胸骨旁主动脉瓣短轴': 0,'胸骨旁乳头肌平面': 0, '胸骨旁短轴': 0,'胸骨旁左心室长轴': 1,'心尖四腔心': 2,'剑突下四腔心': 3,'剑突下下腔静脉长轴': 4,'剑下腔静脉短轴': 5,'LP': 6, 'RL': 7, 'LU': 8, 'LL': 9, 'RD': 10, 'LD': 11, 'RU': 12, 'RB': 13, 'LB': 14,  'RP': 15,'其他': 16}
 # reverse_dic = {value:key for key,value in dic_label.items()}
 import json
 import collections
-with open("train_annotation.json",'r') as f:
+with open("echotrain_annotation.json",'r') as f:
     train_dic = json.load(f)
-with open("test_annotation.json",'r') as f:
+with open("echotest_annotation.json",'r') as f:
     test_dic = json.load(f)
 
-train_label = [view_2_label[train_itm['view']] for train_itm in train_dic]
-test_label = [view_2_label[test_itm['view']] for test_itm in test_dic]
+train_label = [echo_view_2_label[train_itm['view']] for train_itm in train_dic]
+test_label = [echo_view_2_label[test_itm['view']] for test_itm in test_dic]
 
 train_count = collections.Counter(train_label)
-traindic = {reverse_dic[number]:value for number,value in train_count.items()}
+traindic = {echo_label_2_view[number]:value for number,value in train_count.items()}
 print(traindic)
 test_count = collections.Counter(test_label)
-testdic = {reverse_dic[number]:value for number,value in test_count.items()}
+testdic = {echo_label_2_view[number]:value for number,value in test_count.items()}
 print(testdic)
 # %%
